fbf1717e5cc57d5450aca04e2ab6d8bd2aaaabf2
sssallias <top.chel.05@mail.ru>
2024-04-06 18:18:16 +0300

    fixed validation

--- src/utils/validation/register/index.tsx
+++ src/utils/validation/register/index.tsx
@@ -6,9 +6,9 @@ export class RegisterResponse {
     public success: boolean
     public errorsFields: string[]
 
-    constructor(message:string, succes:boolean, errorsFields:string[]) {
+    constructor(message:string, success:boolean, errorsFields:string[]) {
         this.message = message
-        this.success = succes
+        this.success = success
         this.errorsFields = errorsFields
     }
 
@@ -17,11 +17,15 @@ export class RegisterResponse {
 export const registerValidate = (data:RegisterDto) => {
     const errorsFields:string[] = []
     Object.keys(data).forEach(key => {
-        if (new String(data[key as keyof RegisterDto]).length === 0 || !data[key as keyof RegisterDto]) {
+        if (String(data[key as keyof RegisterDto]).length === 0 || !data[key as keyof RegisterDto]) {
             errorsFields.push(key)
         }        
     })
-    if (errorsFields.length !== 0) return new RegisterResponse('Не все поля заполнены!', false, errorsFields)
-    if (data.password !== data.repeatPassword) return new RegisterResponse('Пароли не совпадают!', false, ['password', 'repeatPassword'])
+    if (errorsFields.length !== 0) {
+        return new RegisterResponse('Не все поля заполнены!', false, errorsFields)
+    }
+    if (data.password !== data.repeatPassword) {
+        return new RegisterResponse('Пароли не совпадают!', false, ['password', 'repeatPassword'])
+    }
     return new RegisterResponse('Успешно!', true, errorsFields)
 }
