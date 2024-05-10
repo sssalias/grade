2f995f63ff97aaf42d4208e85487a8da97025384
sssalias <top.chel.05@mail.ru>
2024-05-10 16:58:41 +0300

    Created Mobile version

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -6,16 +6,28 @@ import burgerWhite from 'src/assets/img/icons/burger/burger_white.svg'
 import burgerLime from 'src/assets/img/icons/burger/burger_lime.svg'
 import Icon from 'src/components/atoms/icon'
 import {useAsideStore} from 'src/store/useAsideStore'
+import { useEffect, useState } from 'react'
+import { getWindowSize } from 'src/utils/getWindowSize'
 
 const Header = () => {
 
     const {onOpen} = useAsideStore()
 
+    const [windowSize, setWindowSize] = useState(getWindowSize())
+
+    useEffect(() => {  
+      window.addEventListener('resize', () => setWindowSize(getWindowSize()))
+  
+      return () => {
+        window.removeEventListener('resize', () => setWindowSize(getWindowSize()))
+      }
+    }, [])
+
     return (
         <header className={classes.container}>
             <div className={classes.wrapper}>
                 <Button onClick={onOpen} variant='icon' icon={<Icon path={burgerWhite}/>} iconHover={<Icon path={burgerLime}/>}></Button>
-                <Logo size='sm'/>
+                {windowSize.innerWidth > 480 ? <Logo size='sm'/> : null}
             </div>
         </header>
     )
--- src/components/pages/global/index-page/index.tsx
+++ src/components/pages/global/index-page/index.tsx
@@ -5,15 +5,29 @@ import classes from 'src/components/pages/global/index-page/style.module.scss'
 import Button from 'src/components/atoms/button'
 import AdvantagesList from 'src/components/molecules/advantages/advantages-list'
 import {useNavigate} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getWindowSize } from 'src/utils/getWindowSize'
+
 
 const IndexPage = () => {
 
     const navigate = useNavigate()
 
+    const [windowSize, setWindowSize] = useState(getWindowSize())
+
+    useEffect(() => {  
+      window.addEventListener('resize', () => setWindowSize(getWindowSize()))
+  
+      return () => {
+        window.removeEventListener('resize', () => setWindowSize(getWindowSize()))
+      }
+    }, [])
+
+
     return (
         <MainTemplate>
             <div className={classes.wrapper}>
-                <Logo size='bg'/>
+                <Logo size={windowSize.innerWidth > 480 ? 'bg' : 'sm' }/>
                 <div className={classes.info__wrapper}>
                     <section className={classes.actions}>
                         <SearchField placeholder='Поиск по сайту...'/>
